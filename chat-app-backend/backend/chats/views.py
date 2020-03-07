7ae7b19098082c54829742c917948e1b9aea6e32
Sam Flomenberg <sam.flomenberg@gmail.com>
2020-03-06 19:16:23 -0500

    feat: add healthcheck for backend

--- chat-app-backend/backend/chats/views.py
+++ chat-app-backend/backend/chats/views.py
@@ -1,6 +1,9 @@
 #from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world. You're at the chats index.")
+
+def health(request):
+    return JsonResponse({'status': 200, 'message': 'success'})
