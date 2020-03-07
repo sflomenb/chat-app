6f169bbf34835f539686d947252d7e704af2e5e6
Sam Flomenberg <sam.flomenberg@gmail.com>
2020-03-07 10:55:04 -0500

    feat(models): set message creation time to auto_now

--- chat-app-backend/backend/chats/models.py
+++ chat-app-backend/backend/chats/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
 class Message(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=200)
-    creation_time = models.DateTimeField('creation time')
+    creation_time = models.DateTimeField('creation time', auto_now=True)
 
     def __str__(self):
         return f'{self.author} said {self.content} at {self.creation_time}'
