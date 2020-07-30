79a6b565d4d7d821fe3049c93ae8c1e4032486c2
Sam Flomenberg <sam.flomenberg@gmail.com>
2020-07-30 18:58:21 -0400

    stype: add newlines

--- chat-app-backend/backend/chats/models.py
+++ chat-app-backend/backend/chats/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class User(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
 
 class Message(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
