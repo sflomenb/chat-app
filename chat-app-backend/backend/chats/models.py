27b77ade1d44caca5c1afd36718d93100dad97bd
Sam Flomenberg <sam.flomenberg@gmail.com>
2020-08-01 13:30:20 -0400

    feat(backend): make User name field unique

--- chat-app-backend/backend/chats/models.py
+++ chat-app-backend/backend/chats/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.pk} | {self.name}'
 
 
 class Message(models.Model):
