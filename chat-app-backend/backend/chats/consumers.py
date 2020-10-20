8496ba6e28d82809f1b563b7caefb61d6944b900
Sam Flomenberg <sam.flomenberg@gmail.com>
2020-10-19 22:53:27 -0400

    feat(backend): change to async consumer

--- chat-app-backend/backend/chats/consumers.py
+++ chat-app-backend/backend/chats/consumers.py
@@ -1,32 +1,30 @@
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.group_name = "my-group"
         self.groups.append(self.group_name)
+        print("self.groups: " + str(self.groups))
 
-        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
 
-        self.accept()
+        await self.accept()
 
-    def notify(self, event):
-        self.send(event["content"])
-
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name,
         )
+        print("self.groups: " + str(self.groups))
 
-    def receive(self, text_data):
-        async_to_sync(self.channel_layer.group_send)(
+    async def receive(self, text_data):
+        await self.channel_layer.group_send(
             self.group_name, {"type": "chat_message", "message": text_data}
         )
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event["message"]
 
         # Send message to WebSocket
-        self.send(text_data=message)
+        await self.send(text_data=message)
